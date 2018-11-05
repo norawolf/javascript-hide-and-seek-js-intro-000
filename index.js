68794e00f4f61ccdec3300b0b254e20b059720cb
Nora Vogt <noraevogt@gmail.com>
2018-11-05 23:00:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const lis = document
+  const list = document
   .getElementById('app')
   .querySelectorAll('ul.ranked-list li');
 
-for (let i = 0; i < lis.length; i++) {
-  lis[i].innerHTML = (i + n).parseInt(n);
+for (let i = 0; i < list.length; i++) {
+  list[i].innerHTML = (i + n).parseInt(n);
 }
 }
