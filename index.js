038c3d1ccbda68bd64c7a02660cb95a9e8da7446
Nora Vogt <noraevogt@gmail.com>
2018-11-05 22:59:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function increaseRankBy(n) {
   .getElementById('app')
   .querySelectorAll('ul.ranked-list li');
 
-for (let i = 0; i < lis.length; i++) {
+for (let i = 1; i < lis.length; i++) {
   lis[i].innerHTML = (i + n).toString();
 }
 }
